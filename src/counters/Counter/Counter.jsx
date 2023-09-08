fc30f4d72f7ff0e469fa4ebd6731322845550e08
Michaël Naxara <michael.naxara@cdbdx.biz>
2023-09-08 16:57:07 +0200

    feat: styled counter

--- src/counters/Counter/Counter.jsx
+++ src/counters/Counter/Counter.jsx
@@ -1,15 +1,32 @@
 import { number } from "prop-types";
+import {
+  Button,
+  ButtonGroup,
+  CardActions,
+  CardContent,
+  Typography,
+} from "@mui/material";
+import { Card } from "./styles";
+
 import { useCounter } from "../../hooks";
 
 export default function Counter({ initialValue = 0, step = 1 }) {
   const [counter, { increment, decrement }] = useCounter(initialValue, step);
 
   return (
-    <>
-      <p>{counter}</p>
-      <button onClick={decrement}>{`- ${step}`}</button>
-      <button onClick={increment}>{`+ ${step}`}</button>
-    </>
+    <Card elevation={5}>
+      <CardContent>
+        <Typography variant="h2" component="p">
+          {counter}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <ButtonGroup variant="outlined">
+          <Button onClick={decrement}>{`- ${step}`}</Button>
+          <Button onClick={increment}>{`+ ${step}`}</Button>
+        </ButtonGroup>
+      </CardActions>
+    </Card>
   );
 }
 
---
+++ src/counters/Counter/styles.js
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+import { Card as MuiCard } from "@mui/material/styles";
+
+export const Card = styled(MuiCard)({
+  maxWidth: 200,
+  margin: "16px 0",
+  padding: "8px",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+});
