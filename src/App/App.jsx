aba19a05f9d61ad0160f1b209228b1c9c04262c7
Michaël Naxara <michael.naxara@cdbdx.biz>
2023-09-11 14:27:31 +0200

    feat: 🎸 add css normalize

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -3,10 +3,12 @@ import { RouterProvider } from "react-router-dom";
 
 import { router } from "./router";
 import { theme } from "./theme";
+import { CssBaseline } from "@mui/material";
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
+      <CssBaseline />
       <RouterProvider router={router} />
     </ThemeProvider>
   );
